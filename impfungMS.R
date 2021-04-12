5cd11aaf41de38b1a795ccd6c081ffbf9aaa8140
haemaostra <79973107+haemaostra@users.noreply.github.com>
2021-04-12 16:34:26 +0200

    Add files via upload

---
+++ impfungMS.R
@@ -0,0 +1,44 @@
+rm(list=ls()) 
+
+setwd("C:/Users/NP/Nextcloud/R_und_rtrim/ornitho_rare_telegram")
+
+library(telegram.bot)
+library(rvest)
+library(xml2)
+
+#### Building an R Bot in 3 steps ----
+# 1. Creating the Updater object
+updater <- Updater(token = bot_token("RTelegramBot"))
+
+bot <- updater[["bot"]]
+
+# Get bot info
+print(bot$getMe())
+
+# Get updates
+updates <- bot$getUpdates()
+
+# Retrieve your chat id
+# Note: you should text the bot before calling `getUpdates`
+#chat_id <- updates[[1L]]$from_chat_id() #1 für bot 2 für gruppenchat
+chat_id <- 92150075
+
+#### scrap rare observation ----
+impfzentrumMSpage <- "https://impfzentrum-bbi-stadt-muenster.ticket.io/?onlyTag=erstimpfung"
+impfzentrumMS <- read_html(impfzentrumMSpage)
+impfzentrumMS
+str(impfzentrumMS)
+
+txt_obs <- impfzentrumMS %>% 
+  rvest::html_nodes('body') %>% 
+  xml2::xml_find_all("//p[contains(@class, 'lead text-muted')]") %>% 
+  rvest::html_text()
+
+txt_obs_old <- "There are no active events at the moment."
+
+txt_update <- "NEUE TERMINE IMPFZENTRUM"
+
+# send message if list is updated
+if (txt_obs_old!=txt_obs) {
+  bot$sendMessage(chat_id = chat_id, text = txt_update, parse_mode = "Markdown")
+}
